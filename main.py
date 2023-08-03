d446665754478a267d2ca264cef569327d126117
dovczitter <dov_czitter@yahoo.com>
2023-08-03 17:43:44 -0400

    First commit

---
+++ main.py
@@ -0,0 +1,246 @@
+from kivymd.app import MDApp
+from kivy.clock import Clock
+from kivymd.uix.boxlayout import MDBoxLayout
+import time
+
+from kivy.uix.scrollview import ScrollView
+from kivymd.uix.list import MDList, OneLineListItem
+
+from kivy.uix.floatlayout import FloatLayout
+from kivy.factory import Factory
+from kivy.properties import ObjectProperty
+from kivy.uix.popup import Popup
+from kivy.utils import platform
+
+from recorder import Recorder
+
+mp3Recorder = Recorder()
+loadFilename = ''
+emailFileMsg = ''
+        
+# ============================================
+#               Mp3Recorder
+# ============================================
+class Mp3Recorder(MDBoxLayout):
+        
+    def __init__(self, **kwargs):
+        global mp3Recorder
+        
+        super(Mp3Recorder, self).__init__(**kwargs)
+        #
+        self.state = 'ready'
+        self.time_started = False
+        
+        # ------------ ScrollView stuff. ie 'Info' -----------------
+        self.sv = ScrollView()
+        self.ml = MDList()
+        self.sv.add_widget(self.ml)
+        self.contacts = []
+                     
+        self.file_choose_root = Root()
+        
+        mp3Recorder.ask_permissions()
+        rsp = self.wifiCheck()
+        print(f'after self.wifiCheck(), [{rsp}]')
+        
+        self.start_time()
+
+    # ----------------- timer   ------------------------ #
+    def timer(self, *args):
+        from os.path import exists
+        global loadFilename
+        global emailFileMsg
+
+        time_str = f'Mp3Recorder {time.asctime()}'
+        chk = ''
+        # -------- TODO - log transition ----------- 
+        if self.wifiCheck():
+            chk = '* UP *'
+            self.ids.time_label.color = "orange"
+        else:
+            chk = '- DN -'
+            self.ids.time_label.color = "yellow"
+
+        wifi_str = f'Wifi {chk}'
+        self.ids.time_label.text = f'[{time_str}] [{wifi_str}]'
+        
+        if exists(loadFilename):
+            self.update_labels()
+
+    def start_time(self):
+        Clock.schedule_interval(self.timer, 1)
+
+    def wifiCheck(self):
+        from ping3 import ping, verbose_ping
+        # https://github.com/kyan001/ping3
+        # UP rsp : 0.016164541244506836
+        # DN rsp : None
+        rsp = ping('google.com')
+        return isinstance(rsp, float)
+        
+    # ======================
+    #       record 
+    # ======================
+    def record(self):
+        global mp3Recorder
+        self.state = mp3Recorder.record(self.state)
+        self.update_labels()
+
+    # ======================
+    #       email 
+    # ======================
+    def email(self):
+        global mp3Recorder
+
+        msg = ''
+        if self.state != 'ready':
+            msg = 'Recording in progress.'
+        else:
+            recordFilename = mp3Recorder.get_mp3_filename()
+            msg = mp3Recorder.email(recordFilename)
+            
+        self.ids.container.add_widget(
+            OneLineListItem(text=msg)
+        )
+        self.update_labels()
+
+    # ======================
+    #       emailfile 
+    # ======================
+    def emailfile(self):
+        if self.state != 'ready':
+            self.ids.container.add_widget(
+                OneLineListItem(text='Recording in progress.')
+            )
+            self.update_labels()
+            return
+        
+        # ------------------------------------------------------------------------
+        # Root emailfile class will do the actual filechoose and email.
+        # See show_load() stuff.
+        # 'loadFilename' reported in timer() via update_labels() when available.
+        # ------------------------------------------------------------------------
+        self.file_choose_root.show_load()
+            
+        self.update_labels()
+        
+    # ======================
+    #       exit 
+    # ======================
+    def exit(self):
+        global mp3Recorder
+        mp3Recorder.exit()
+
+    # ======================
+    #       update_labels
+    # ======================
+    def update_labels(self):
+        from os.path import basename, exists
+        global mp3Recorder
+        global loadFilename
+
+        # --------- Button label updates --------
+        if self.state == 'ready':
+            self.ids.record_button.text = 'START Recording'
+
+        if self.state == 'recording':
+            self.ids.record_button.text = 'STOP Recording'
+            
+        # -------- Update header --------------
+        self.ids.container.add_widget(
+            OneLineListItem(text=f'[ ========== {time.asctime()} ========== ]')
+        )
+
+        # -------- Email and EmailFile updates
+        recordFilename = mp3Recorder.get_mp3_filename()
+        if exists(recordFilename):
+            basefn = basename(recordFilename)
+            end_msg = f'[Audio : {self.state}] [Recorded File : {basefn}] '
+            self.ids.container.add_widget(
+                OneLineListItem(text=end_msg)
+            )
+            self.sv.scroll_to(end_msg)
+
+        if exists(loadFilename):
+            basefn = basename(loadFilename)
+            end_msg = f'[Email File : {basefn}] '
+            self.ids.container.add_widget(
+                OneLineListItem(text=end_msg)
+            )
+            self.sv.scroll_to(end_msg)
+            # NOTE - clear loacal loadFilename, generates only one update per load.
+            loadFilename = ''
+            
+# ============================================
+#               LoadDialog
+# ============================================
+class LoadDialog(FloatLayout):
+    
+    def sort_by_date(files, filesystem):    
+        import os
+        return (sorted(f for f in files if filesystem.is_dir(f)) +
+            sorted((f for f in files if not filesystem.is_dir(f)), key=lambda fi: os.stat(fi).st_mtime, reverse = True))
+        
+    def sort_by_name(files, filesystem):
+        return (sorted(f for f in files if filesystem.is_dir(f)) +
+                sorted(f for f in files if not filesystem.is_dir(f))) 
+        
+    default_sort_func = ObjectProperty(sort_by_date)
+          
+    emailfile = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+    sort = ObjectProperty(None)
+    
+# ============================================
+#               Root
+# ============================================
+class Root(FloatLayout):
+
+    def dismiss_popup(self):
+        self._popup.dismiss()
+
+    def show_load(self):
+        import os
+        
+        content = LoadDialog(emailfile=self.emailfile, cancel=self.dismiss_popup)
+        PATH = "."
+        if platform == "android":
+            from android.permissions import request_permissions, Permission
+            
+            request_permissions([Permission.READ_EXTERNAL_STORAGE, Permission.WRITE_EXTERNAL_STORAGE])
+            app_folder = os.path.dirname(os.path.abspath(__file__))
+            # Mp3Recorder mp3 files
+            PATH = "/storage/emulated/0/Music/Mp3Recorder"
+        content.ids.filechooser.path = PATH
+
+        self._popup = Popup(title="Load file", content=content, size_hint=(0.9, 0.9))
+        self._popup.open()
+ 
+    def emailfile(self, path, selection):
+        from os.path import exists
+
+        global mp3Recorder
+        global loadFilename
+        global emailFileMsg
+        msg = ''
+        try:
+            loadFilename = selection[0]
+            if exists(loadFilename):
+                emailFileMsg = mp3Recorder.email(loadFilename)
+            else:
+                emailFileMsg = f'Email File error, file [{loadFilename}] does not exist'
+            self.dismiss_popup()
+        except:
+            pass
+        
+class Mp3RecorderApp(MDApp):
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Orange"
+        return Mp3Recorder()
+
+Factory.register('Root', cls=Root)
+Factory.register('LoadDialog', cls=LoadDialog)
+
+if __name__ == '__main__':
+    Mp3RecorderApp().run()
---
+++ recorder.py
@@ -0,0 +1,309 @@
+from jnius import autoclass
+from kivy import Logger, platform
+from datetime import datetime
+import os
+
+# https://github.com/kivy/python-for-android/pull/2725
+# https://stackoverflow.com/questions/73909410/manage-external-storage-vs-write-external-storage
+# https://github.com/kivy/buildozer/issues/1004
+# https://developer.android.com/reference/android/os/Environment
+
+Environment = autoclass('android.os.Environment')
+
+from sharedstorage import SharedStorage
+#
+PythonActivity = None
+if platform == "android":
+    from android import mActivity, autoclass, api_version
+    from android.permissions import request_permissions, Permission
+
+    from android.storage import app_storage_path
+    from android.storage import primary_external_storage_path
+    from android.storage import secondary_external_storage_path
+
+    PythonActivity = autoclass("org.kivy.android.PythonActivity").mActivity
+    ExternalStoragePath = PythonActivity.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS).getPath()
+    DownLoadsPath = PythonActivity.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS).getPath()
+    Context = autoclass('android.content.Context')
+
+# =========================================================== #
+#                   class Recorder                         #
+# =========================================================== #
+
+class Recorder():
+
+    required_permissions = [
+            Permission.RECORD_AUDIO,
+            Permission.WRITE_EXTERNAL_STORAGE,
+            Permission.READ_EXTERNAL_STORAGE,
+# Wifi test:            
+            Permission.ACCESS_WIFI_STATE, 
+            Permission.INTERNET, 
+    ]
+
+    def __init__(self, **kwargs):
+        super(Recorder, self).__init__(**kwargs)
+
+        if not platform == "android":
+            return
+        
+        self.MediaRecorder = autoclass('android.media.MediaRecorder')
+        self.AudioSource = autoclass('android.media.MediaRecorder$AudioSource')
+        self.OutputFormat = autoclass('android.media.MediaRecorder$OutputFormat')
+        self.AudioEncoder = autoclass('android.media.MediaRecorder$AudioEncoder')
+        #
+        self.mp3_filename = ''
+        self.mp3Fn = ''
+        self.config = dict()
+        self.EMAIL_USERNAME = ''
+        self.EMAIL_PASSWORD = ''
+        self.EMAIL_SENDER = ''
+        self.EMAIL_RECEIVER = ''
+        self.SERVER_HOST = ''
+        self.SERVER_PORT = 0
+            
+    # ----------------- configInit ------------------------ #        
+    def configInit(self):
+        
+        from os.path import exists
+        import shutil
+        import traceback
+
+        self.config.clear()
+            
+        print('START # ----------------- configInit ------------------------ #')
+        # ==============================================================================================
+        # Note for local editing Mp3Recorder.cvs file [problems due to file extension and write restrictions in 'Documents']:
+        # Name the new local edited file 'Mp3Recorder.apk' [or '.txt', not '.csv']
+        # Copy via explorer 'Mp3Recorder.apk' to the Documents folder
+        # Delete the old 'Mp3Recorder.csv' [or rename to something with a '.csv' ext, ie 'Mp3RecorerOld.csv']
+        # Rename the new 'Mp3Recorder.apk' file to 'Mp3Recorder.csv', now you're done.
+        # ==============================================================================================
+        SharePath = './Mp3Recorder.csv'
+        ConfigPath = '/storage/emulated/0/Documents/Mp3Recorder/Mp3Recorder.csv' 
+        path = ''  
+        content_list = []    
+        try:
+            
+            # Move a local copy and read config from here, may be locally modified.
+            # https://www.tutorialspoint.com/How-to-copy-files-from-one-folder-to-another-using-Python
+            documents_dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).getPath()
+            ConfigPath = f'{documents_dir}/Mp3Recorder/Mp3Recorder.csv'
+            if exists(ConfigPath) == False:
+                ss = SharedStorage()
+                share = ss.copy_to_shared(SharePath)
+                path = ss.copy_from_shared(share)
+                
+            with open(ConfigPath) as f:
+                content_list = f.readlines()
+            f.close()
+
+            for item in content_list:
+                i = item.replace('\n','')
+                print(f'[{i}]')
+                itemList = []
+                try:
+                    itemList = item.replace(' ','').split(',')
+                except ValueError:
+                    continue
+                if len(itemList) > 1 and itemList[0][0] != '#':
+                    # cleanup NL etc
+                    for i,s in enumerate(itemList):
+                        itemList[i] = s.strip()
+                    k = itemList[0].strip()
+                    v = itemList[1:]
+                    self.config[k] = v
+                    print(f'[ {k}:{v} ]')
+                    
+            if self.config['Username']:
+                self.EMAIL_USERNAME = str(self.config['Username'][0])
+                print(f'self.EMAIL_USERNAME: {self.EMAIL_USERNAME}')
+            if self.config['Password']:
+                self.EMAIL_PASSWORD = str(self.config['Password'][0])
+                print(f'self.EMAIL_PASSWORD: {self.EMAIL_PASSWORD}')
+            if self.config['Sender']:
+                self.EMAIL_SENDER = str(self.config['Sender'][0])
+                print(f'self.EMAIL_SENDER: {self.EMAIL_SENDER}')
+            if self.config['Receiver']:
+                self.EMAIL_RECEIVER = (','.join(self.config['Receiver']))
+                print(f'self.EMAIL_RECEIVER: {self.EMAIL_RECEIVER}')
+            if self.config['Host']:
+                self.SERVER_HOST = str(self.config['Host'][0])
+                print(f'self.SERVER_HOST: {self.SERVER_HOST}')
+            if self.config['Port']:
+                self.SERVER_PORT = int(self.config['Port'][0])
+                print(f'self.SERVER_PORT: {self.SERVER_PORT}')
+                
+        except Exception as error:
+            self.config.clear()
+            err = traceback.format_exc()
+            print(' ================================ configInit traceback START ================================ ')
+            print(err)
+            print(' ================================ configInit traceback END ================================ ')
+   
+    # ----------------- permissions ------------------------ #        
+    def check_permission(permission, activity=None):
+        if platform == "android":
+            activity = PythonActivity
+        if not activity:
+            return False
+
+        permission_status = 0
+        permission_granted = 0 == permission_status
+        return permission_granted
+
+    def ask_permission(self,permission):
+        PythonActivity.requestPermissions([permission])
+        if len(self.config) == 0:
+            self.configInit()
+
+    def ask_permissions(self):
+        request_permissions(self.required_permissions)
+        if len(self.config) == 0:
+            self.configInit()
+
+    def check_required_permission(self):
+        permissions = self.required_permissions
+        #
+        has_permissions = True
+        for permission in permissions:
+            if not self.check_permission(permission):
+                has_permissions = False
+        return has_permissions
+            
+    def create_recorder(self):
+        now = datetime.now()
+        dt_string = now.strftime("%d%b%Y_%H%M%S")
+        self.mp3Fn = f'Mp3Recorder_{dt_string}.mp3'
+
+        self.recorder = self.MediaRecorder()
+        self.recorder.setAudioSource(self.AudioSource.MIC)
+        self.recorder.setOutputFormat(self.OutputFormat.MPEG_4)
+        self.recorder.setOutputFile(self.mp3Fn)
+        self.recorder.setAudioEncoder(self.AudioEncoder.AAC)
+        self.recorder.prepare()
+
+    def get_recorder(self):
+        if not hasattr(self, "recorder"):
+            self.create_recorder()
+        return self.recorder
+
+    def remove_recorder(self):
+        delattr(self, "recorder")
+   
+    # ----------------- record_start ------------------------ #
+    def record_start(self):
+        self.get_recorder()
+        
+        self.mp3_filename = ''
+        
+        if self.check_required_permission():
+            # - new
+            self.create_recorder()
+            # - 
+            self.recorder.start()
+        else:
+            self.ask_permissions()
+    
+    # ----------------- record_stop ------------------------ #
+    def record_stop(self):
+        self.get_recorder()
+        self.recorder.stop() 
+      
+        # Copy the mp3 to Main Storage:
+        ss = SharedStorage()
+        share = ss.copy_to_shared(self.mp3Fn)
+        path = ss.copy_from_shared(share)
+        self.mp3_filename = path
+
+        self.recorder.reset()
+        self.recorder.release()
+        # we need to do this in order to make the object reusable
+        self.remove_recorder()
+          
+    # ----------------- get_mp3_filename ------------------------ #
+    def get_mp3_filename(self):
+        return self.mp3_filename
+    
+    # ======================
+    #       record 
+    # ======================
+    def record(self,state):
+        if state == 'ready':
+            state = 'recording'
+            if len(self.config) == 0:
+                self.configInit()
+            self.record_start()
+        elif state == 'recording':
+            state = 'ready'
+            self.record_stop()
+        return state
+
+    # ======================
+    #       email 
+    # ======================
+
+    def send_email(self, filename):
+        import smtplib
+        from email.mime.multipart import MIMEMultipart
+        from email.mime.text import MIMEText
+        from email.mime.base import MIMEBase
+        from email import encoders
+        from os.path import exists
+
+        if filename == None or not exists(filename):
+            return f' Error: [{filename}] does not exist.'
+        
+        basefn = os.path.basename(filename)
+        
+        # set up the SMTP server
+        server = smtplib.SMTP(self.SERVER_HOST, self.SERVER_PORT)
+        server.ehlo()
+        server.starttls()
+        server.ehlo()
+        
+        print(f'SERVER_HOST |{self.SERVER_HOST}|')
+        print(f'SERVER_PORT |{self.SERVER_PORT}|')
+        print(f'EMAIL_USERNAME |{self.EMAIL_USERNAME}|')
+        print(f'EMAIL_PASSWORD |{self.EMAIL_PASSWORD}|')
+        print(f'EMAIL_RECEIVER |{self.EMAIL_RECEIVER}|')
+        
+        server.login(self.EMAIL_USERNAME, self.EMAIL_PASSWORD)
+        
+        # setup payload
+        msg = MIMEMultipart()
+        msg['From'] = self.EMAIL_SENDER
+        msg['To'] = self.EMAIL_RECEIVER
+
+        # Subject
+        msg['Subject'] = f'[{basefn}] From Mp3Recorder'
+
+        # Body
+        body = f'[{basefn}] From Mp3Recorder'
+        msg.attach(MIMEText(body, 'plain'))
+
+        # file attachment
+        attachment = open(filename, "rb")
+        p = MIMEBase('application', 'octet-stream')
+        p.set_payload((attachment).read())
+        encoders.encode_base64(p)
+        p.add_header('Content-Disposition', "attachment; filename= %s" % basefn)
+        msg.attach(p)
+        
+        #Send the mail
+        text = msg.as_string()
+        server.sendmail(self.EMAIL_SENDER, self.EMAIL_RECEIVER, text)
+        server.quit()        
+        
+        return f'[{basefn}] email complete.'
+    
+    # ======== email ========
+    def email(self, filename):
+        return self.send_email(filename)
+
+    # ======================
+    #       exit 
+    # ======================
+    def exit(self):
+        quit()
+
